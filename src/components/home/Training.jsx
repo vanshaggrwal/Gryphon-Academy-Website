ceee05189772720d0c91fddf8cd4117c7ca52b1b
Vansh Singla <vanshsingla@Vanshs-MacBook-Air.local>
2026-03-24 11:16:00 +0530

    Training height width seeteed

--- src/components/home/Training.jsx
+++ src/components/home/Training.jsx
@@ -2,7 +2,7 @@ import trainEvaImage from "../../assets/traineva.avif";
 
 export default function Training() {
   return (
-    <section className="h-screen w-full bg-[#f4f5f9]">
+    <section className="w-full min-h-[100svh] h-[100dvh] bg-[#f4f5f9]">
       <div className="h-full w-full px-0">
         <div className="h-full w-full overflow-hidden border-y border-[#e5e7ef] bg-white">
           <img
